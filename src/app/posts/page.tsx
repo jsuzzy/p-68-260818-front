d074b85da710df954d47e44227deb4c93dd591b1
jeongsuji <sujijeong11@gmail.com>
2026-08-18 14:28:58 +0900

    type compile

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState, useEffect } from "react";
+
+interface Post {
+    id: number;
+    title: string;
+}
+
+export default function Posts() {
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/v1/posts")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setPosts(data);
+            });
+    }, []);
+
+    return (
+        <ul>
+            {
+                posts.map((p) => <li key={p.id}>{p.title}</li>)
+            }
+        </ul>
+    )
+}
