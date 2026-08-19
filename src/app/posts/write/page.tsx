a7ec51c6f702a614c97852e08b701729951072cc
jeongsuji <sujijeong11@gmail.com>
2026-08-19 12:47:15 +0900

    write request

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -1,36 +1,56 @@
 'use client';
 
 import { fetchApi } from "@/lib/client";
+import { useRouter } from "next/navigation";
 
 export default function Write() {
 
-    const onSubmitHandle = (e: any)=>{
-        e.preventDefault(); //기본 기능 막음 -> 제출 버튼 눌러도 전송 안 됨
+    const router = useRouter();
+
+    const onSubmitHandle = (e: any) => {
+        e.preventDefault();
 
         const form = e.target;
 
         const titleValue = form.title.value;
         const contentValue = form.content.value;
 
-        if(titleValue.length === 0){
+        if (titleValue.length === 0) {
             alert("제목을 입력해 주세요.");
             form.title.focus();
             return;
         }
 
-        if(contentValue.length === 0){
+        if (contentValue.length === 0) {
             alert("내용을 입력해 주세요.")
             form.content.focus();
             return;
         }
 
-        
+        //백엔드에 저장 요청
+        fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "title": titleValue,
+                "content": contentValue
+            })
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            alert(data.msg);
+            router.replace(`/posts/${data.data.id}`);
+        })
+
 
     }
 
     return (
         <div>
-            <h1>글 작성 페이지</h1>
+            <h1 className="text-center">글 작성 페이지</h1>
             <form
                 onSubmit={onSubmitHandle}
                 className="flex flex-col gap-4">
